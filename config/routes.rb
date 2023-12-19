1edf1b0c00d74cb33252d85d279e7a49eebca5da
trndav <farsiteguide@gmail.com>
2023-12-19 09:34:19 +0100

    Twitter app authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#root"
+
+  get "/auth/:provider/callback", to: "sessions#create"
   post "/auth/:provider/callback", to: "sessions#create"
 
   get "up" => "rails/health#show", as: :rails_health_check
